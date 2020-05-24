f9c9ac15891f2164c24976191c82826662eb1797
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-05-24 21:07:40 +0530

    Added MealsNavigator Component

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+
+import MealsNavigator from './navigation/MealsNavigator';
 
 const fetchfonts = () => {
   return(
@@ -25,9 +27,7 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator />
   );
 }
 
