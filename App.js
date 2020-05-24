0c1deefa801bd26d3e72512ae65c92012767fc29
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-05-24 19:44:08 +0530

    Fetch Fonts

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchfonts = () => {
+  return(
+    Font.loadAsync({
+      'open-sans': require("./assets/fonts/OpenSans-Regular.ttf"),
+      'open-sans-bold': require("./assets/fonts/OpenSans-Bold.ttf")
+    })
+  );
+};
 
 export default function App() {
   return (
