1025179b0f9c864e760ef5cabe982198b568db58
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-05-24 21:07:05 +0530

    Shifted createStackNavigator to react navigation stack

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,15 +1,16 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
-import CategoryMealsScreen from '../screen/CategoryMealsScreen';
-import MealDetailScreen from '../screen/MealDetailScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
 
 const MealsNavigator = createStackNavigator({
-    Categories: CategoriesScreen,
-    CategoryMeals: {
-        screen: CategoryMealsScreen
-    },
-    MealDetail: MealDetailScreen
+	Categories: CategoriesScreen,
+	CategoryMeals: {
+		screen: CategoryMealsScreen
+	},
+	MealDetail: MealDetailScreen
 });
 
-export default MealsNavigator;
+export default createAppContainer(MealsNavigator);
