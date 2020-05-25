196ca8bea0eae6eaa0096dc90ef244c931edde4e
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-05-25 19:06:21 +0530

    Added push() pop() functions to buttons

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -11,6 +11,9 @@ const CategoryMealsScreen = props => {
                     props.navigation.navigate({routeName: 'MealDetail'})
                 }}
             />
+            <Button title="Go Back" onPress={() => {
+                props.navigation.pop();
+            }} />
         </View>
     );
 };
