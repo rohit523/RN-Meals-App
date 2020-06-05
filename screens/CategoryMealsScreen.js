89230825b7a17e2042192143146d43ac6f757576
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-06-05 22:32:19 +0530

    Dynamic Navigation Options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoryMealsScreen = props => {
     const catId = props.navigation.getParam('categoryId');
@@ -23,6 +24,20 @@ const CategoryMealsScreen = props => {
             }} />
         </View>
     );
+};
+
+CategoryMealsScreen.navigationOptions = navData => {
+    const catId = navData.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    };
 };
 
 const styles = StyleSheet.create({
