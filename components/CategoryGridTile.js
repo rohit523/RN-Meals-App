29fbfe775f23bf08823bc7a55f5e5bf87a60217c
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-06-05 23:14:01 +0530

    Added basic skeleton structure to CategoryGridTile

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+
+const CategoryGridTile = props => {
+
+};
+
+const style = StyleSheet.create({});
+
+export default CategoryGridTile;
