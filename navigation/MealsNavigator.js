5c3e44f8f83d65176a432062d56669e2d237da9c
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-06-05 23:07:25 +0530

    Default Navigation Options

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,11 @@
+import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator({
 	Categories: {
@@ -13,6 +15,14 @@ const MealsNavigator = createStackNavigator({
 		screen: CategoryMealsScreen
 	},
 	MealDetail: MealDetailScreen
+}, {
+	// initialRouteName: 'MealDetail',
+	defaultNavigationOptions: {
+		headerStyle: {
+			backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+		},
+		headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+	}
 });
 
 export default createAppContainer(MealsNavigator);
