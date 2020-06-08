df1aca92dcf19ce723376075285f7776b5752612
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-06-08 19:01:14 +0530

    Added MealItem Component to Favorites Screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import MealList from '../components/MealList';
+import { MEALS } from '../data/dummy-data';
+
 const FavoritesScreen = props => {
+    const FavMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id == 'm2');
     return(
-        <View style={styles.screen}>
-            <Text>The Favorites Screen!</Text>
-        </View>
+        <MealList listData={FavMeals} navigation={props.navigation} />
     );
 };
 
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
+FavoritesScreen.navigationOptions = {
+    headerTitle: 'Your Favorites'
+};
 
 export default FavoritesScreen;
