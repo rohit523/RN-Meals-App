28a3dfed5c659bd80773f1c8bfe3fe7c2ba7edfe
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-06-09 23:50:00 +0530

    More Navigation Config & Styling

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { craeteDrawerNavigator, createDrawerNavigator } from 'react-navigation-drawer';
@@ -17,6 +17,12 @@ import Colors from '../constants/Colors';
 const defaultStackNavOptions = {
 	headerStyle: {
 		backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+	},
+	headerTitleStyle: {
+		fontFamily: 'open-sans-bold'
+	},
+	headerBackTitleStyle: {
+		fontFamily: 'open-sans'
 	},
 	headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 }
@@ -51,7 +57,8 @@ const tabScreenConfig = {
 					<Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
 				)
 			},
-			tabBarColor: Colors.primaryColor
+			tabBarColor: Colors.primaryColor,
+			tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Meals</Text> : 'Meals'
 		}
 	},
 	Favorites: {
@@ -63,7 +70,8 @@ const tabScreenConfig = {
 					<Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
 				)
 			},
-			tabBarColor: Colors.accentColor
+			tabBarColor: Colors.accentColor,
+			tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Favorites!</Text> : 'Favorites'
 		}
 	}
 }
@@ -77,6 +85,9 @@ const MealsFavTabNavigator =
 		}) 
 		: createBottomTabNavigator(tabScreenConfig, {
 			tabBarOptions: {
+				labelStyle: {
+					fontFamily: 'open-sans'
+				},
 				activeTintColor: Colors.accentColor
 			}
 		});
