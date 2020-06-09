db9be426d3c75d38fc0d7745b5709c499d6f405a
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-06-09 23:13:25 +0530

    Added HeaderButtons to Favorites Screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
 import { MEALS } from '../data/dummy-data';
 
@@ -11,8 +13,21 @@ const FavoritesScreen = props => {
     );
 };
 
-FavoritesScreen.navigationOptions = {
-    headerTitle: 'Your Favorites'
-};
+FavoritesScreen.navigationOptions = navData => {
+    return {
+      headerTitle: 'Your Favorites',
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item 
+                title="Menu" 
+                iconName='ios-menu' 
+                onPress={() => {
+                    navData.navigation.toggleDrawer();
+                }} 
+            />
+        </HeaderButtons>
+      )
+    };
+  };
 
 export default FavoritesScreen;
