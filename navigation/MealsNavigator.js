0192fa22612c525cb4e1931e2c6890b55ecc47f1
Rohit Vinod <2017.vinod.rohit@ves.ac.in>
2020-06-09 21:30:04 +0530

    Added Drawer Navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { craeteDrawerNavigator, createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
@@ -10,6 +11,7 @@ import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
+import FilterScreen from '../screens/FiltersScreen';
 import Colors from '../constants/Colors';
 
 const defaultStackNavOptions = {
@@ -79,4 +81,13 @@ const MealsFavTabNavigator =
 			}
 		});
 
-export default createAppContainer(MealsFavTabNavigator);
+const FiltersNavigator = createStackNavigator({
+	Filters: FilterScreen
+});		
+
+const MainNavigator = createDrawerNavigator({
+	MealsFav: MealsFavTabNavigator,
+	Filters: FiltersNavigator
+});
+
+export default createAppContainer(MainNavigator);
